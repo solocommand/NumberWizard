4f76905a01ce90a459702ad3ce4941cf8352aa29
Josh Worden <solocommand@gmail.com>
2017-08-25 16:32:19 -0500

    Input

--- Assets/Scripts/NumberWizard.cs
+++ Assets/Scripts/NumberWizard.cs
@@ -6,12 +6,25 @@ public class NumberWizard : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		int min = 1;
+		int max = 1000;
+
 		print("Welcome to Number Wizard!");
-		print("Pick a number in your head, but don't tell me.");
+		print("Pick a number between " + min + " and " + max + " and I'll guess it for you! Ready?");
+		print("Press RETURN for a correct guess, UP if my guess is too low, and DOWN if I guessed too high.");
+
 	}
 
 	// Update is called once per frame
 	void Update () {
-
+		if (Input.GetKeyDown(KeyCode.UpArrow)) {
+			print("UP");
+		}
+		if (Input.GetKeyDown(KeyCode.DownArrow)) {
+			print("DOWN");
+		}
+		if (Input.GetKeyDown(KeyCode.Return)) {
+			print("RETURN");
+		}
 	}
 }
