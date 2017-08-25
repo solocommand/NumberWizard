f4f52f9194ddb8ae8c5860db7b3d2554d39f90e8
Josh Worden <solocommand@gmail.com>
2017-08-25 17:04:57 -0500

    refactor

--- Assets/Scripts/NumberWizard.cs
+++ Assets/Scripts/NumberWizard.cs
@@ -4,27 +4,45 @@ using UnityEngine;
 
 public class NumberWizard : MonoBehaviour {
 
+	int min;
+	int max;
+	int guess;
+
 	// Use this for initialization
 	void Start () {
-		int min = 1;
-		int max = 1000;
-
-		print("Welcome to Number Wizard!");
-		print("Pick a number between " + min + " and " + max + " and I'll guess it for you! Ready?");
-		print("Press RETURN for a correct guess, UP if my guess is too low, and DOWN if I guessed too high.");
-
+		StartGame();
 	}
 
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.UpArrow)) {
-			print("UP");
+			min = guess;
+			Next();
+		} else if (Input.GetKeyDown(KeyCode.DownArrow)) {
+			max = guess;
+			guess = (max + min) / 2;
+			Next();
+		} else if (Input.GetKeyDown(KeyCode.Return)) {
+			print("I won!");
+			StartGame();
 		}
-		if (Input.GetKeyDown(KeyCode.DownArrow)) {
-			print("DOWN");
-		}
-		if (Input.GetKeyDown(KeyCode.Return)) {
-			print("RETURN");
-		}
+	}
+
+	void StartGame()
+	{
+		min = 1;
+		max = 1000;
+		print("┌===========================┐");
+		print("| Welcome to Number Wizard! |");
+		print("└===========================┘");
+		print("Pick a number between " + min + " and " + max + " and I'll guess it for you! Ready?");
+		max = max + 1;
+		Next();
+	}
+
+	void Next() {
+		guess = (max + min) / 2;
+		print("Higher or lower than " + guess + "?");
+		print("Press RETURN for a correct guess, UP if my guess is too low, and DOWN if I guessed too high.");
 	}
 }
